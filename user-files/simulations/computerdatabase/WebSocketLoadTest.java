a53e1edf131118eb8f4b439d2012c1f01286f1d8
ManjeetSharma <sharmaboymanjeet12@gmail.com>
2023-10-09 23:43:28 +0530

    commit

--- user-files/simulations/computerdatabase/WebSocketLoadTest.java
+++ user-files/simulations/computerdatabase/WebSocketLoadTest.java
@@ -7,12 +7,11 @@ import static io.gatling.recorder.internal.bouncycastle.oer.its.ieee1609dot2.bas
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 class WebSocketLoadTest extends Simulation {
     HttpProtocolBuilder httpProtocol = http
             .baseUrl("https://chatqa.clovedental.in") // Replace with the WebSocket URL's base URL
             .acceptHeader("application/json");
+
     ScenarioBuilder users  = scenario("WebSocket Load Test")
             .exec(http("WebSocket Connection")
                     .get("/wss2/socket") // Replace with the WebSocket path
@@ -22,15 +21,18 @@ class WebSocketLoadTest extends Simulation {
 
             .exec(http("WebSocket Interaction")
                     .post("/wss2/socket") // Replace with the WebSocket path
-                    .header("Upgrade", "websocket")
-                    .header("Connection", "Upgrade")
-                    .header("Sec-WebSocket-Key", "dGhlIHNhbXBsZSBub25jZQ==")
-                    .header("Sec-WebSocket-Version", "13")
+                    .header("platform", "android")
+                    .header("appVersion", "3.1.3")
+                    .header("platformVersion", "33")
+                    .header("userId", "14")
+                    .header("current_chat_id", "0")
+                    .header("authToken", "W3WNF%2BGgnXSGZV%2Bp8uJFKLzVqk3dJs5s2cCpjVe0Yzk%3D")
+                    .header("connected_in", "2")
+                    .header("deviceId", "3e5830ea-d0a9-4e66-968d-fd8314730a4a")
                     .body(StringBody(""))
                     .check(status().is(101))) // Check if WebSocket interaction is successful
-
             .pause(5, seconds) ;
-    // Adjust the pause duration as needed
+
     {
         setUp(
                 users.injectOpen(atOnceUsers(10)) // Adjust the number of users as needed
